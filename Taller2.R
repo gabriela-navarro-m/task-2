c5db3ecd9553017389d3a61960a35e605b1aaaf1
daquinterov1 <da.quinterov1@uniandes.edu.co>
2021-04-23 14:31:34 -0500

    Avance primer punto

--- Taller2.R
+++ Taller2.R
@@ -19,11 +19,77 @@ for (i in (1:74)){
 }
 
 # 1.2. Asegúrese de crear una variable tipo_delito que almacene el tipo de delito.
-delito_homicidios = list(c(lista_df[[1:10]]))
-tipo_delito <- list(lista_df[[1:10]],lista_df[[11:13]],lista_df[[14:22]],lista_df[[22:29]],lista_df[[29:39]],lista_df[[40]],lista_df[41:48])
-tipo_delito <- list("")
+ # Para no tener que agregar todo individualmente cree un loop que me agregre a una variables las
+  # Listas que quiero
+# Voy a dividir los delitos por variables y despues lo agrego en una sola variable
+
+# HOMICIDIOS
+homicidios <- list()
+for (a in (1:10)){
+  homicidios[a] <- list(lista_df[a])
+}
+
+# HURTO A ENTIDADES COMERCIALES
+numero = 0
+hurto_entidades_comerciales <- list()
+for (b in c((11:13),(23:29))){
+  numero = numero + 1
+  hurto_entidades_comerciales[numero] <- list(lista_df[b])
+}
+
+# HURTO A ENTIDADES FINANCIERAS
+numero = 0
+hurto_entidades_financieras <- list()
+for (c in (14:22)){
+  numero = numero + 1
+  hurto_entidades_financieras[numero] <- list(lista_df[c])
+}
+
+# DELITO AUTOMORES
+numero = 0
+delito_automores <- list()
+for (d in (30:39)){
+  numero = numero + 1
+  delito_automores[numero] <- list(lista_df[d])
+}
+
+# DELITO MOTOCICLETAS
+numero = 0
+delito_motocicletas <- list()
+for (e in c((54:60),40)){
+  numero = numero + 1
+  delito_motocicletas[numero] <- list(lista_df[e])
+}
+
+# HURTO DE PERSONAS
+numero = 0
+hurtos_personas <- list()
+for (f in (41:53)){
+  numero = numero + 1
+  hurtos_personas[numero] <- list(lista_df[f])
+}
+
+# LESIONES PERSONALES
+numero = 0
+lesiones_personales <-list()
+for (g in c(61:74)){
+  numero = numero + 1
+  hurtos_personas[numero] <- list(lista_df[g])
+}
+
+tipo_delito <- list()
+tipo_delito[[1]] <- homicidios
+tipo_delito[[2]] <- hurto_entidades_comerciales
+tipo_delito[[3]] <- hurto_entidades_comerciales
+tipo_delito[[4]] <- delito_automores
+tipo_delito[[5]] <- delito_motocicletas
+tipo_delito[[6]] <- hurtos_personas
+tipo_delito[[7]] <- lesiones_personales
 
 # 1.3. Use la función rbindlist de la librería data.table para crear un dataframe que contenga todos los elementos
   # de la lista. Asegúrese de llamar a este objeto df.
+
+
+
 
   
